7c0c8cdde1fc3598d8e1e78bf3feb6cda0beb062
Ben Mizrahi <benm@item-soft.com>
2024-01-29 21:08:27 +0000

    rename & fix

--- tests/test_e2e_test.py
+++ tests/test_e2e_test.py
@@ -9,7 +9,7 @@ def test_e2e_process(*args, **kwargs):
     This is a test function for end-to-end processing.
     """
     Paperless(
-        notebook_path=f'gs://pdp-cicd-test/test_go_gcs/prd/TopApps/test.ipynb').\
+        notebook_path=f'./resources/test.ipynb').\
         configure().\
         wait_for_session().\
         verify().\
