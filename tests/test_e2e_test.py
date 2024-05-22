4219f05be5d498539064ed17df3a0182b2da7fe5
sa <ben.mizrahi@plarium.com>
2024-05-22 10:47:49 +0300

    update

--- tests/test_e2e_test.py
+++ tests/test_e2e_test.py
@@ -9,7 +9,7 @@ def test_e2e_process(*args, **kwargs):
     This is a test function for end-to-end processing.
     """
     Paperless(
-        notebook_path=f'./resources/test.ipynb').\
+        notebookPath=f'./tests/resources/test.ipynb').\
         configure().\
         wait_for_session().\
         verify().\
